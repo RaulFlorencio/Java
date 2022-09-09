37ea421a2220ff06482ff612df997947dc87ab99
Raul Florencio <91292875+RaulFlorencio@users.noreply.github.com>
2022-09-09 12:49:31 -0300

    Add files via upload

---
+++ Nova pasta/SintaxeVariavelEFluxo.java
@@ -0,0 +1,141 @@
+package com.mycompany.sintaxe.variavel.e.fluxo;
+
+public class SintaxeVariavelEFluxo {
+
+// QUASE TUDO APRENDIDO NA PRIMEIRA FASE DO CURSO JAVA DO ALURA
+    /* public static void main (String args[]){
+        double salario = 1250.00;
+        System.out.println("meu salario e " +salario);
+        double divisao = 237 / 2;
+        System.out.println(divisao);
+        double outraTentiva;
+        outraTentiva = 5.0 /2;
+        System.out.println(outraTentiva);
+    }*/
+ /*public static void main(String args[]){
+        double salario = 1270.50;
+        int valor = (int)salario;
+        System.out.println(valor);
+    
+    
+    }*/
+ /* public static void main(String[] args) {
+        char letra = 'a';
+        System.out.println(letra);
+        char valor = 65;
+        System.out.println(valor);
+        valor = (char) (valor + 1);
+        System.out.println(valor);
+        String palavra = "Bem vindo a o codigo do Raul";
+        System.out.println(palavra);
+        palavra = palavra + 2022;
+        System.out.println(palavra);
+   
+        
+        
+    }*/
+ /*public static void main(String[] args) {
+        System.out.println("Testando ifs");
+        int idade = 16;
+        int quantidadePessoas = 3;
+        if (idade >= 18) {
+            System.out.println("Voce tem mais de 18 anos");
+        } else {
+            if (quantidadePessoas >= 2) {
+                System.out.println("Voce nao tem idade, mas pode entrar, pois estar acompanhado");
+            } else {
+                System.out.println("voce nao pode entrar, pois e de menor e nao esta acompanhado");
+            }
+
+        }
+
+    }*/
+ /*public static void main(String[] args) {
+        System.out.println("Testando ifs, utilizando dois tipos de argumentos no if");
+        int idade = 18;
+        int quantidadePessoas = 2;
+        boolean acompanhado = quantidadePessoas >=2;
+        if (idade >= 18 && acompanhado ) {
+            System.out.println("Seja bem vindo");
+
+        } else {
+            System.out.println("infelizmente voce nao pode entrar");
+        }
+
+    }*/
+ /*public static void main(String[] args) {
+        System.out.println("Testando ifs, utilizando dois tipos de argumentos no if");
+        int idade = 18;
+        int quantidadePessoas = 2;
+        boolean acompanhado = quantidadePessoas >=2;
+        if (idade >= 18 && acompanhado ) {
+            System.out.println("Seja bem vindo");
+
+        } else {
+            System.out.println("infelizmente voce nao pode entrar");
+        }
+
+    }*/
+ /*public static void main(String[] args) {
+        double salario = 3300.00;
+        if (salario >= 1900 && salario <= 2800) {
+            System.out.println("A sua aliquota é de 7.5%");
+            System.out.println("Você pode deduzir até R$ 142");
+        } else {
+            if (salario >= 2800.01 && salario <= 3751.0) {
+                System.out.println("A sua aliquota é de 15%");
+                System.out.println("Você pode deduzir até R$ 350");
+
+            } else {
+                if (salario >= 3751.01 && salario <= 4664.00) {
+                    System.out.println("A sua aliquota é de 22.5%");
+                System.out.println("Você pode deduzir até R$ 636");
+
+                }
+
+            }
+
+        }
+    }*/
+ /*public static void main(String[] args) {
+        int c = 0;
+        while(c<=10){
+            System.out.println(c);
+            c = c + 1;
+        
+    }
+        
+    }*/
+ /* public static void main(String[] args) {
+        for(int contador=0;contador<=10;contador++) {
+            System.out.println(contador);
+
+        }
+    }*/
+ /*public static void main(String[] args) {
+        for (int contador = 1; contador <= 10; contador++) {
+            for (int multiplicador = 0; multiplicador <= 10; multiplicador++) {
+                System.out.print(contador * multiplicador);
+                System.out.print(" ");
+
+            }
+            System.out.println();
+        }
+    }*/
+    /*public static void main(String[] args) {
+        for (int linha = 0; linha < 10; linha++) {
+            for (int coluna = 0; coluna <= linha; coluna++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }*/
+    
+    /*public static void main(String[] args) {
+        for(int contador=3;contador<=100;contador+=3){
+            System.out.println(contador);
+        
+    }
+    }/*
+}
